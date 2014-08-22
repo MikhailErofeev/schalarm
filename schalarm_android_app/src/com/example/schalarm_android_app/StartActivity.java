0a7e70c400628b64f479271b6c6e63d265747927
FFX20413 <whoosh.s@gmail.com>
2014-08-22 21:08:56 +0400

    lets start

---
+++ schalarm_android_app/src/com/example/schalarm_android_app/StartActivity.java
@@ -0,0 +1,12 @@
+package com.example.schalarm_android_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
